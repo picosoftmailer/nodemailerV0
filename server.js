40b2b892bcaa0bb9e141641bc7f3c97348f1e426
picosoftmailer <49859987+picosoftmailer@users.noreply.github.com>
2019-04-24 10:04:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const crone = require ('./controllers/process_cron')
 const uploadTemplateRoutes = require('./routes/uploadTemplate');
 const uploadAttachmentRoutes = require('./routes/uploadAttachment');
 const createXmlMailRoutes = require('./routes/createXmlMail');
-const dbRoutes =require('./routes/db');
 const app = express();
+const WebServicegetFolder = require ('C:\\Users\\ayoub.bellaj\\Desktop\\nodemailerV0\\routes\\getFolder.js')
+const WebServicegriddb= require ('C:\\Users\\ayoub.bellaj\\Desktop\\nodemailerV0\\routes\\griddb.js');
+
 
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -17,16 +19,15 @@ app.use(bodyParser.json());
 app.use((req,res,next)=>{
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With, Content-Type, Accept, Authorization');
-    
     if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'PUT,POST,PATH,GET,DELETE');
         return res.status(200).json({});
     }
-
     next();
 });
 
-app.use(dbRoutes);
+WebServicegetFolder(app);
+WebServicegriddb(app);
 app.use(uploadTemplateRoutes);
 app.use(uploadAttachmentRoutes);
 app.use(createXmlMailRoutes);
